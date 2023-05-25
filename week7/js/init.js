f8b544390324aca3f81c0d27736c1ef887cfc813
Joanne Luong <joanneluong@PersonalMicrosoftSoftware.ucla.edu>
2023-05-24 19:30:01 -0700

    week8

--- week7/js/init.js
+++ week7/js/init.js
@@ -4,12 +4,12 @@ let mapOptions = {'center': [34.0709,-118.444],'zoom':9}
 // use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 
-let Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+let Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 });
 
-Stadia_OSMBright.addTo(map)
+Stadia_AlidadeSmooth.addTo(map)
 
 
 
