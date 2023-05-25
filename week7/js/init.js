a0bffcefa6d094cf3ef5185044ba6e492435a1a5
Joanne Luong <joanneluong@PersonalMicrosoftSoftware.ucla.edu>
2023-05-24 19:34:10 -0700

    week8

--- week7/js/init.js
+++ week7/js/init.js
@@ -4,12 +4,15 @@ let mapOptions = {'center': [34.0709,-118.444],'zoom':9}
 // use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 
-let Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+let Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
 	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+	ext: 'png'
 });
 
-Stadia_AlidadeSmooth.addTo(map)
+Stamen_TonerLite.addTo(map)
 
 
 
